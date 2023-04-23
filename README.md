21ab57a4eb254c2011ada83c7438d24e0cf4d52e
hwh998 <qwe102339@gmail.com>
2023-04-22 17:46:11 -0700

    query plan option

--- README.md
+++ README.md
@@ -1,19 +1,18 @@
 # COSC504-Project
-
-## Basic Query Performance result 
-
+In this project, we expect to compare the query performance on two famous graph databases, Neo4j and AllegreDB with the WIKI dataset. 
 
 ## Reproduction of the experiment with neo4j 
 Assuming you have a neo4j free account, please create an instance by clicking `new instance` and then choose `empty instance`. Download the credentials for the instance to connect to the DB later on. 
-Once the instance is up and running, you can go ahead and upload three datasets, including the chameleon, crocodile, and squirrel. 
+Once the instance is up and running, you can go ahead and upload one of the three datasets, including the chameleon, crocodile, and squirrel. 
 To reproduce the experiment result, you need to upload their .dump files that we prepared beforehand  in the /Data/WIKI_neo4j/ directory, and we go into the instance and click on the `Import Database`. 
 
 
-After you successfully import the data, we can run the Neo4j.py code by `python ./src/Neo4j.py 0 777`,  which will run the experiment on the squirrel dataset with the node id as 777. 
+After you successfully import the data, we can run the Neo4j.py code by `python ./src/Neo4j.py 0 777 0`,  which will run the experiment on the squirrel dataset with the node id as 777. 
 The first argument 0 indicates which dataset it's going to run on, and 0, 1, and 2 indicate the squirrel, chameleon, and crocodile, respectively.
 The second argument 777 specifies which node id, and you can try a different id around 1 to 2000.
+The last argument 0 indicates whether we want to look into the query plan, and 0 means don't show the query plan, while 1 means show the query plan.
 
-Note that when you run the specific dataset, you need to upload the .dump file to the neo4j database, and `reset to blank` before you upload another dataset because there is a node and relationship limitation in neo4j auraDB.
+Note that when you want to run the query on a particular dataset, you need to `reset to blank` before you upload the corresponding dataset because there is a node and relationship limitation in neo4j auraDB.
 
 
 
--- src/Neo4j.py
+++ src/Neo4j.py
@@ -32,7 +32,7 @@ class Neo4jApp:
 		# close the connection to the database
 		self.driver.close()
 
-	def query1(self,data_op, id):
+	def query1(self,data_op, id, profile):
 		print("***1st order query.")
 		# Returns the 1st order query.
 		with self.driver.session(database="neo4j") as session:
@@ -45,18 +45,19 @@ class Neo4jApp:
 			
 			result = session.run(query1)
 			print([row for row in result])
-
-			#Get hte query profile performance.
-			query1_profile="PROFILE "+query1
-			print(query1_profile)
-			Profile_result = session.run(query1_profile)
-			summary = Profile_result.consume()
-			print(summary.profile['args']['string-representation'])
+			
+			if(profile == '1'):
+				#Get hte query profile performance.
+				query1_profile="PROFILE "+query1
+				print(query1_profile)
+				Profile_result = session.run(query1_profile)
+				summary = Profile_result.consume()
+				print(summary.profile['args']['string-representation'])
 
 			return[row for row in result]
 
 
-	def query2(self,data_op,id):
+	def query2(self,data_op, id, profile):
 		print("***2rd order query.")
 
 		# Run the 2nd order query.
@@ -75,16 +76,17 @@ class Neo4jApp:
 			result = session.run(query2)
 			print([row for row in result])
 
-			# Get hte query profile performance.
-			query2_profile="PROFILE "+query2
-			Profile_result = session.run(query2_profile)
-			summary = Profile_result.consume()
-			print(summary.profile['args']['string-representation'])
+			if(profile == '1'):
+				# Get hte query profile performance.
+				query2_profile="PROFILE "+query2
+				Profile_result = session.run(query2_profile)
+				summary = Profile_result.consume()
+				print(summary.profile['args']['string-representation'])
 			return[row for row in result]	
 	
 	
 	
-	def query3(self, data_op,id):
+	def query3(self, data_op,id, profile):
 		print("***3rd order query.")
 		# Run the 3rd order query.
 		with self.driver.session(database="neo4j") as session:
@@ -101,17 +103,17 @@ class Neo4jApp:
 
 			result = session.run(query3)
 			print([row for row in result])
-
-			# Get hte query profile performance.
-			query3_profile="PROFILE "+query3
-			Profile_result = session.run(query3_profile)
-			summary = Profile_result.consume()
-			print(summary.profile['args']['string-representation'])
+			if(profile == '1'):
+				# Get hte query profile performance.
+				query3_profile="PROFILE "+query3
+				Profile_result = session.run(query3_profile)
+				summary = Profile_result.consume()
+				print(summary.profile['args']['string-representation'])
 		return [row for row in result]			
 	
 	
 
-	def query4(self,data_op,id):
+	def query4(self,data_op,id, profile):
 		print("***4th order query.")
 		# Run the 4th order query.
 		with self.driver.session(database="neo4j") as session:
@@ -127,14 +129,15 @@ class Neo4jApp:
 			result = session.run(query4)
 			print([row for row in result])
 
-			# Get hte query profile performance.
-			query4_profile="PROFILE "+query4
-			Profile_result = session.run(query4_profile)
-			summary = Profile_result.consume()
-			print(summary.profile['args']['string-representation'])
+			if(profile == '1'):
+				# Get hte query profile performance.
+				query4_profile="PROFILE "+query4
+				Profile_result = session.run(query4_profile)
+				summary = Profile_result.consume()
+				print(summary.profile['args']['string-representation'])
 		return [row for row in result]			
 
-	def query5(self,data_op,id):
+	def query5(self,data_op,id, profile):
 		# Run the 5th order query.
 		print("***5th order query.")
 		with self.driver.session(database="neo4j") as session:
@@ -142,21 +145,21 @@ class Neo4jApp:
 				query5=("MATCH (c:squirrel {id:'"+id+"'}) -[:s_conn]-()-[:s_conn]-()-[:s_conn]-()-[:s_conn]-()-[:s_conn] -(r) RETURN COUNT(r);")
 			elif(data_op=='1'):
 				query5=("MATCH (c:chameleon {id:'"+id+"'}) -[:cham_conn]-()-[:cham_conn]-()-[:cham_conn]-()-[:cham_conn]-()-[:cham_conn] -(r) RETURN COUNT(r);")
-				#("MATCH (c:chameleon {id:'777'}) -[:cham_conn]-()-[:cham_conn]-()-[:cham_conn]-()-[:cham_conn] -(r) RETURN COUNT(r);")
 			else:
 				query5=("MATCH (c:crocodile {id:'"+id+"'}) -[:CONS]-()-[:CONS]-()-[:CONS]-()-[:CONS]-()-[:CONS] -(r) RETURN COUNT(r);")
 
 			result = session.run(query5)
 			print([row for row in result])
 
-			# Get hte query profile performance.
-			query5_profile="PROFILE "+query5
-			Profile_result = session.run(query5_profile)
-			summary = Profile_result.consume()
-			print(summary.profile['args']['string-representation'])
+			if(profile == '1'):
+				# Get hte query profile performance.
+				query5_profile="PROFILE "+query5
+				Profile_result = session.run(query5_profile)
+				summary = Profile_result.consume()
+				print(summary.profile['args']['string-representation'])
 		return [row for row in result]			
 	
-	def query6(self,data_op,id):
+	def query6(self,data_op,id, profile):
 		print("***6th order query.")
 		# Run the 6th order query.
 		with self.driver.session(database="neo4j") as session:
@@ -164,26 +167,48 @@ class Neo4jApp:
 				query6=("MATCH (c:squirrel {id:'"+id+"'}) -[:s_conn]-()-[:s_conn]-()-[:s_conn]-()-[:s_conn]-()-[:s_conn]-()-[:s_conn] -(r) RETURN COUNT(r);")
 			elif(data_op=='1'):
 				query6=("MATCH (c:chameleon {id:'"+id+"'}) -[:cham_conn]-()-[:cham_conn]-()-[:cham_conn]-()-[:cham_conn]-()-[:cham_conn]-()-[:cham_conn] -(r) RETURN COUNT(r);")
-				#("MATCH (c:chameleon {id:'777'}) -[:cham_conn]-()-[:cham_conn]-()-[:cham_conn]-()-[:cham_conn]-()-[:cham_conn] -(r) RETURN COUNT(r);")
 			else:
 				query6=("MATCH (c:crocodile {id:'"+id+"'}) -[:CONS]-()-[:CONS]-()-[:CONS]-()-[:CONS]-()-[:CONS]-()-[:CONS] -(r) RETURN COUNT(r);")
-				#("MATCH (c:crocodile {id:'777'}) -[:CONS]-()-[:CONS]-()-[:CONS]-()-[:CONS]-()-[:CONS] -(r) RETURN COUNT(r);")
 	
 			result = session.run(query6)
 			print([row for row in result])
-
-			#Get the query profile performance.
-			query6_profile="PROFILE "+query6
-			Profile_result = session.run(query6_profile)
-			summary = Profile_result.consume()
-			print(summary.profile['args']['string-representation'])
+			
+			if(profile == '1'):
+				#Get the query profile performance.
+				query6_profile="PROFILE "+query6
+				Profile_result = session.run(query6_profile)
+				summary = Profile_result.consume()
+				print(summary.profile['args']['string-representation'])
 		return [row for row in result]			
-
+	def datasetoverview(self,data_op, profile):
+		print("===== Overview of data =====\n")
+		# Run the 6th order query.
+		with self.driver.session(database="neo4j") as session:
+			if(data_op=='0'):
+				data_o = ("MATCH (p:squirrel)-[r:s_conn]-(f:squirrel) RETURN count(DISTINCT p) as numOfNodes, count(DISTINCT r) as numOfEdges, count(DISTINCT r)/((count(DISTINCT p)-1) * (count(DISTINCT p) - 1.0)) AS graphDensity")
+			elif(data_op=='1'):
+				data_o = ("MATCH (p:chameleon)-[r:cham_conn]-(f:chameleon) RETURN count(DISTINCT p) as numOfNodes, count(DISTINCT r) as numOfEdges, count(DISTINCT r)/((count(DISTINCT p)-1) * (count(DISTINCT p) - 1.0)) AS graphDensity")
+			else:
+				data_o = ("MATCH (p:crocodile)-[r:CONS]-(f:crocodile) RETURN count(DISTINCT p) as numOfNodes, count(DISTINCT r) as numOfEdges, count(DISTINCT r)/((count(DISTINCT p)-1) * (count(DISTINCT p) - 1.0)) AS graphDensity")
+	
+			result = session.run(data_o)
+			print([row for row in result])
+			print("============================\n")	
+			#whether to profile your query
+			if(profile == '1'):
+				#Get the query profile performance.
+				query6_profile="PROFILE "+data_o
+				Profile_result = session.run(query6_profile)
+				summary = Profile_result.consume()
+				print(summary.profile['args']['string-representation'])
+		return [row for row in result]	
 if __name__ == "__main__":
 	# Aura queries use an encrypted connection using the "neo4j+s" URI scheme
-	data_option= sys.argv[1] #0:squirrel ; 1:chameleon ; 2: crocodile.
+	data_option= sys.argv[1] #0:squirrel, 1:chameleon, 2: crocodile.
 	# which id 
-	id = sys.argv[2] # which user to experiment.
+	id = sys.argv[2] # which id to work on.
+	# Profile your query 
+	profile = sys.argv[3] # 1: Profile the query, 2: Don't profile
 	if data_option=='0':
 		data='squirrel'
 	elif data_option=='1':
@@ -194,11 +219,11 @@ if __name__ == "__main__":
 	app.connect()
 
 	print("===== Data set",data, ".=====")
-	#app.loadData() # Run only once to avoid duplicate data.
-	app.query1(data_option,id)
-	app.query2(data_option,id)
-	app.query3(data_option,id)
-	app.query4(data_option,id)
-	app.query5(data_option,id)
-	#app.query6(data_option,id)
+	app.datasetoverview(data_option, profile)
+	app.query1(data_option,id, profile)
+	app.query2(data_option,id, profile)
+	app.query3(data_option,id, profile)
+	app.query4(data_option,id, profile)
+	app.query5(data_option,id, profile)
+	app.query6(data_option,id, profile)
 	app.close()
